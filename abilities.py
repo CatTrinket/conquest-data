92f97e30e87fe3686e8cee90e87d7ce033fca35f
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2012-07-02 21:07:11 -0400

    Clean up abilities.py, sort of.

--- abilities.py
+++ abilities.py
@@ -1,22 +1,23 @@
-abilities = open('/tmp/conquest/fsroot/data/Tokusei.dat', 'rb')
-pokemons = open('/tmp/conquest/fsroot/data/Pokemon.dat', 'rb')
+from struct import unpack
+
+def parse_abilities(abilities):
+    return [
+        abilities >> 24 & 0x7f,
+        abilities >> 17 & 0x7f,
+        (abilities >> 10 & 0x3f) | (abilities & 1) << 6
+    ]
+
+ability_data = open('/tmp/conquest/fsroot/data/Tokusei.dat', 'rb')
+pokemon_data = open('/tmp/conquest/fsroot/data/Pokemon.dat', 'rb')
 
 ability_names = []
-while True:
-    ability = abilities.read(20)
-    if not ability:
-        break
+for ability in range(128):
+    name, = unpack('15s5x', ability_data.read(20))
+    ability_names.append(name.rstrip(b'\x00').decode('ASCII'))
 
-    ability_names.append(ability[:15].rstrip(b'\x00').decode('ASCII'))
+for pokemon in range(200):
+    name, abilities = unpack('>11s13xL20x', pokemon_data.read(0x30))
+    name = name.rstrip(b'\x00').decode('ASCII')
+    abilities = [ability_names[a] for a in parse_abilities(abilities)]
 
-while True:
-    pokemon = pokemons.read(0x30)
-    if not pokemon:
-        break
-
-    name = pokemon[:11].rstrip(b'\x00').decode('ASCII')
-
-    pokemon_abilities = (pokemon[24], pokemon[25] >> 1,
-        pokemon[26] >> 2 | (pokemon[27] & 1) << 6)
-    print('{0:10}: {1}'.format(name, ', '.join(ability_names[a] for a in
-                                               pokemon_abilities)))
+    print('{0:10}: {1}'.format(name, ', '.join(abilities)))
