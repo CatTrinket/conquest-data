f23f1e39ec08a8fec339da1034e2c0773b36fea9
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2012-07-15 05:19:22 -0400

    Take the underscore off the Ability class's name.

--- abilities.py
+++ abilities.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from struct import unpack
 
-_Ability = namedtuple('ability', ['name', 'mystery'])
+Ability = namedtuple('Ability', ['name', 'mystery'])
 
 abilities = []
 
@@ -9,7 +9,7 @@ with open('/tmp/conquest/fsroot/data/Tokusei.dat', 'rb') as ability_data:
     for ability in range(128):
         name, mystery = unpack('15s5s', ability_data.read(20))
         name = name.decode('Shift_JIS').rstrip('\x00')
-        abilities.append(_Ability(name, mystery))
+        abilities.append(Ability(name, mystery))
 
 if __name__ == '__main__':
     from binascii import hexlify
