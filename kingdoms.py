a5c5289cebc84befaf3f0bc8d8c5c48f57119531
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2012-07-15 16:35:54 -0400

    Find kingdoms' types automatically.
    Or something like that.  I found /something/ that goes up by one with
    each kingdom, which works for types.

--- kingdoms.py
+++ kingdoms.py
@@ -3,15 +3,23 @@ from struct import Struct
 from common import decode_name, types
 
 class Kingdom:
-    struct = Struct('<11s13s')
-    types = iter(types)
+    struct = Struct('<11sB3L')
 
     def __init__(self, raw_struct):
         """Parse everything.  Sort of."""
 
-        self.name, self.mystery = self.struct.unpack(raw_struct)
-        self.name = decode_name(self.name)
-        self.type = next(self.types)
+        data = iter(self.struct.unpack(raw_struct))
+
+        self.name = decode_name(next(data))
+        self.mystery_1 = next(data)
+        self.mystery_2 = next(data)
+        self.mystery_3 = next(data)
+
+        # Type, and some mystery
+        group = next(data)
+        self.mystery_4 = group & 0x7FFF
+        self.type = types[group >> 15 & 0x1F]
+        self.mystery_5 = group >> 20
 
 
 kingdoms = []
@@ -21,7 +29,8 @@ with open('/tmp/conquest/fsroot/data/Kuni.dat', 'rb') as kingdom_data:
         kingdoms.append(Kingdom(kingdom_data.read(0x18)))
 
 if __name__ == '__main__':
-    from binascii import hexlify
+    template = ('{name:11} {mystery_1:08b} {mystery_2:032b} {mystery_3:032b} '
+                '{mystery_5:012b} {type:8} {mystery_4:015b}')
+
     for kingdom in kingdoms:
-        print('{0:11} {1}'.format(kingdom.name,
-          hexlify(kingdom.mystery).decode('ASCII')))
+        print(template.format(**kingdom.__dict__))
